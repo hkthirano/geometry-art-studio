edfeefcec3ce0a77018062ac3222cd0e16ba5902
hkthirano <hirano.hkt@gmail.com>
2025-04-03 23:30:34 +0000

    feat: add API call utility and custom navigation client for MSAL integration

---
+++ frontend/src/utils/ApiCall.ts
@@ -0,0 +1,40 @@
+// import { loginRequest, graphConfig } from "../authConfig";
+// import { msalInstance } from "../index";
+
+export async function callMsGraph() {
+    // const account = msalInstance.getActiveAccount();
+    // if (!account) {
+    //     throw Error("No active account! Verify a user has been signed in and setActiveAccount has been called.");
+    // }
+
+    // const response = await msalInstance.acquireTokenSilent({
+    //     ...loginRequest,
+    //     account: account
+    // });
+
+    // const headers = new Headers();
+    // const bearer = `Bearer ${response.accessToken}`;
+
+    // headers.append("Authorization", bearer);
+
+    // const options = {
+    //     method: "GET",
+    //     headers: headers
+    // };
+
+    // return fetch(graphConfig.graphMeEndpoint, options)
+    //     .then(response => response.json())
+    //     .catch(error => console.log(error));
+
+    // dummy
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                displayName: "John Doe",
+                givenName: "John",
+                surname: "Doe",
+                id: "12345"
+            });
+        }, 1000);
+    });
+}
