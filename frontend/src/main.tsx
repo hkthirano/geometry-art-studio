14c3727764018701047bd3d31901e5fe2173cc93
hkthirano <hirano.hkt@gmail.com>
2025-04-04 03:49:51 +0000

    feat: integrate MSAL authentication in main entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthenticationResult, EventType, PublicClientApplication } from '@azure/msal-browser';
+import { msalConfig } from './authConfig.ts';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export const msalInstance = new PublicClientApplication(msalConfig);
+
+msalInstance.initialize().then(() => {
+  // Default to using the first account if no account is active on page load
+  if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+    // Account selection logic is app dependent. Adjust as needed for different use cases.
+    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+  }
+
+  // Optional - This will update account state if a user signs in from another tab or window
+  msalInstance.enableAccountStorageEvents();
+
+  msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS
+      ||
+      event.eventType === EventType.ACQUIRE_TOKEN_SUCCESS
+      ||
+      event.eventType === EventType.SSO_SILENT_SUCCESS
+    ) {
+      const payload = event.payload as AuthenticationResult;
+      const account = payload.account;
+      msalInstance.setActiveAccount(account);
+    }
+  });
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+});
