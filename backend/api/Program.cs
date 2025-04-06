753331ff020dffcf0434cd1ae11b4dc16b25b612
hokuto.hirano <aaa@bbb.com>
2025-04-06 18:08:25 +0900

    add: permissive CORS policy for development environment

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -14,6 +14,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 }
 
 app.UseHttpsRedirection();
